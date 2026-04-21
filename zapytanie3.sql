ca17500fa49b1433518cdb3e15cafc1f4ec737e4
Wiktor Mencel <91634355+iskrin@users.noreply.github.com>
2026-04-21 18:30:06 +0200

    Update zapytanie3.sql

--- zapytanie3.sql
+++ zapytanie3.sql
@@ -1,14 +1,21 @@
-/* CEL: Znalezienie 10 najbardziej wartościowych klientów na podstawie sumy ich wpłat.*/
+/* CEL: Obliczenie łącznej sprzedaży dla każdego pracownika i porównanie jej ze średnią sprzedażą w firmie.*/
+
+WITH EmployeeSales AS (
+    SELECT 
+        e.employeeNumber,
+        CONCAT(e.firstName, ' ', e.lastName) AS sales_rep,
+        SUM(od.quantityOrdered * od.priceEach) AS total_sales
+    FROM employees e
+    JOIN customers c ON e.employeeNumber = c.salesRepEmployeeNumber
+    JOIN orders o ON c.customerNumber = o.customerNumber
+    JOIN orderdetails od ON o.orderNumber = od.orderNumber
+    GROUP BY e.employeeNumber
+)
 
 SELECT 
-    c.customerName, 
-    c.city, 
-    c.country, 
-    COUNT(o.orderNumber) AS total_orders,
-    SUM(p.amount) AS total_paid
-FROM customers c
-JOIN orders o ON c.customerNumber = o.customerNumber
-JOIN payments p ON c.customerNumber = p.customerNumber
-GROUP BY c.customerNumber
-ORDER BY total_paid DESC
-LIMIT 10;
+    sales_rep,
+    total_sales,
+    (SELECT AVG(total_sales) FROM EmployeeSales) AS company_average,
+    total_sales - (SELECT AVG(total_sales) FROM EmployeeSales) AS performance_diff
+FROM EmployeeSales
+ORDER BY total_sales DESC;
